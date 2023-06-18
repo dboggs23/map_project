c027987e71221dcfe4c1098148927664809f1c29
Dalton Boggs <dalton2369@gmail.com>
2023-06-18 14:55:10 -0500

    added test endpoint

---
+++ backend/backend/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet(Name = "Test")]
+        [Produces("application/json")]
+        public string Test()
+        {
+            return "OK";
+        }
+    }
+}
