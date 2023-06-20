b4ad45429aedb5ee76350e3a89260c05b3cbfc44
Dalton Boggs <dalton2369@gmail.com>
2023-06-19 21:38:18 -0500

    welcome to my page

---
+++ frontend/components/Nav.tsx
@@ -0,0 +1,6 @@
+import Link from 'next/link'
+import NavStyles from './styles/NavStyles'
+
+export default function Nav() {
+	return <NavStyles>{/* <Link href='/products'>Products</Link> */}</NavStyles>
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,118 +1,114 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/components/Header'
+import styled from '@emotion/styled'
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const HomeStyled = styled.div`
+  background: background-color: silver;
+        background-image: linear-gradient(335deg, #b00 23px, transparent 23px),
+        linear-gradient(155deg, #d00 23px, transparent 23px),
+        linear-gradient(335deg, #b00 23px, transparent 23px),
+        linear-gradient(155deg, #d00 23px, transparent 23px);
+        background-size: 58px 58px;
+        background-position: 0px 2px, 4px 35px, 29px 31px, 34px 6px;
+        background-height: 100%;
+        font-family: "Comic Sans MS", "Comic Sans", cursive;
+        `
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const ClippyContainer = styled.div`
+		display: flex;
+		flex-direction: row;
+		padding: 40px;
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		.h1box {
+			background: white;
+		}
+		#shadowBox {
+			background-color: rgb(0, 0, 0);
+			/* Fallback color */
+			background-color: hsla(
+				56.12903225806453,
+				18.562874251496993%,
+				67.25490196078432%,
+				0.959
+			);
+			/* Black w/opacity/see-through */
+			border: 3px solid;
+		}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		.rainbow {
+			text-align: center;
+			text-decoration: underline;
+			font-size: 32px;
+			font-family: monospace;
+			letter-spacing: 5px;
+		}
+		.rainbow_text_animated {
+			background: linear-gradient(
+				to right,
+				#6666ff,
+				#0099ff,
+				#00ff00,
+				#ff3399,
+				#6666ff
+			);
+			-webkit-background-clip: text;
+			background-clip: text;
+			color: transparent;
+			animation: rainbow_animation 6s ease-in-out infinite;
+			background-size: 400% 100%;
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+		@keyframes rainbow_animation {
+			0%,
+			100% {
+				background-position: 0 0;
+			}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			50% {
+				background-position: 100% 0;
+			}
+		}
+
+		.meow {
+			display: block;
+			margin: auto;
+			margin-bottom: 30px;
+		}
+
+		.clippy {
+			max-width: 30%;
+			margin-left: auto;
+		}
+	`
+	return (
+		<HomeStyled>
+			<style global jsx>{`
+				html,
+				body,
+				body > div:first-child,
+				div#__next,
+				div#__next > div {
+					height: 100%;
+				}
+			`}</style>
+
+			<Header></Header>
+			<ClippyContainer>
+				<div id='shadowBox'>
+					<h1 className='rainbow rainbow_text_animated'>Welcome to my page!</h1>
+					<img
+						className='meow'
+						src='/kitten.gif'
+						alt='animation of a kitten sleeping on a couch'
+					/>
+				</div>
+				<img
+					className='clippy'
+					src='/Clippy.jpg'
+					alt='Clippy is a friendly, talking paperclip who will help you navigate the internet.'
+				/>
+			</ClippyContainer>
+		</HomeStyled>
+	)
 }
